97b7f10e8bd14a9a4f5dad756e671d5a06417c52
Piotrosz <piotrosz@gmail.com>
2015-06-16 23:07:39 +0200

    Initial commit (from VS2015)

---
+++ FluentBuilderSample/FluentBuilder.Tests/PersonBuilderWithImplicitCastTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FluentBuilder.Tests
+{
+    public class PersonBuilderWithImplicitCastTests
+    {
+        [Fact]
+        public void ShouldBuildPersonWithFirstNameAndLastName()
+        {
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+
+            Person person = new PersonBuilderWithImplicitCast()
+                .WithFirstName(firstName)
+                .WithLastName(lastName);
+
+            person.FirstName.Should().Be(firstName);
+            person.LastName.Should().Be(lastName);
+        }
+    }
+}
---
+++ FluentBuilderSample/FluentBuilder/PersonBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FluentBuilder
+{
+    public class PersonBuilder : IFluentInterface
+    {
+        private Lazy<Person> person;
+
+        public PersonBuilder()
+        {
+            this.person = new Lazy<Person>(() => new Person());
+        }
+
+        public PersonBuilder WithFirstName(string firstName)
+        {
+            this.person.Value.FirstName = firstName;
+            return this;
+        }
+
+        public PersonBuilder WithLastName(string lastName)
+        {
+            this.person.Value.LastName = lastName;
+            return this;
+        }
+
+        public Person Build()
+        {
+            return this.person.Value;
+        }
+    }
+}
