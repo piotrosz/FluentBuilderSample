5e6a4668f6abf948b139dad622ad1516c63c0a66
Piotrosz <piotrosz@gmail.com>
2015-06-16 23:17:21 +0200

    Revert "Revert "Initial commit (from VS2015)""
    This reverts commit e5cc5847496feffb2808e936e77c5508ab2cd5ad.

---
+++ FluentBuilderSample/FluentBuilder.Tests/PersonBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FluentBuilder.Tests
+{
+    public class PersonBuilderTests
+    {
+        [Fact]
+        public void ShouldBuildPersonWithFirstNameAndLastName()
+        {
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+
+            var result = new PersonBuilder()
+                .WithFirstName(firstName)
+                .WithLastName(lastName)
+                .Build();
+
+            result.FirstName.Should().Be(firstName);
+            result.LastName.Should().Be(lastName);
+        }
+    }
+}
---
+++ FluentBuilderSample/FluentBuilder/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentBuilder
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
